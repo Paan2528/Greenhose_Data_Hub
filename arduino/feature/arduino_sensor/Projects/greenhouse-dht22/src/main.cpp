320bb44603a9b8da751bcf44c807c039a60688d9
Paan2528 <140539606+Paan2528@users.noreply.github.com>
2026-08-05 22:07:42 +0200

    fix: sensor output data in array output for communicate with UART.

--- arduino/feature/arduino_sensor/Projects/greenhouse-dht22/src/main.cpp
+++ arduino/feature/arduino_sensor/Projects/greenhouse-dht22/src/main.cpp
@@ -4,15 +4,15 @@
 #include "soilsensor.h"
 
 // ###### Pins Setting ###########
-fan myFan(7);
-pump myPump(8);
 
-DHT dhtIn(2, DHT11);
-DHT dhtOut(3, DHT11);
+fan actuatorFan(7);
+pump actuatorPump(2);
 
-sensor mySensors(2, 2);
+DHT dhtIn(12, DHT11);
+DHT dhtOut(13, DHT11);
 
-soilsensor soilPinSensor(1, 2, 3);
+sensor tempAndHumdiSensor(12, 13);
+soilsensor soilPinSensor(4, 5, 6);
 
 void setup()
 {
@@ -23,87 +23,101 @@ void setup()
 
 void loop()
 {
-  unsigned long lastCheck = 0;
-  const unsigned long interval = 5UL * 60UL * 1000UL;
-  unsigned long currentMills = millis();
+  // unsigned long lastCheck = 0;
+  // const unsigned long interval = 5UL * 60UL * 1000UL;
+  // unsigned long currentMills = millis();
 
-  if (currentMills - lastCheck >= interval)
+  // if (currentMills - lastCheck >= interval)
+  //{
+  //  ####### read sensor ########
+  if (tempAndHumdiSensor.read())
   {
-    // ####### read sensor ########
-    if (mySensors.read())
-    {
-      Serial.println(mySensors.getTempInside());
-      Serial.println(mySensors.getTempOutside());
-      Serial.println(mySensors.getHumidity());
-    }
-    else
-    {
-      Serial.println(NAN);
-    }
-    // ####### check fan status ########
-    if (myFan.getIsOn())
-    {
-      Serial.println(myFan.getIsOn());
-    }
-    else if (myFan.getIsOff())
-    {
-      Serial.println(myFan.getIsOff());
-    }
-    else
-    {
-      Serial.println(NAN);
-    }
+    Serial.print(tempAndHumdiSensor.getTempInside());
+    Serial.print(",");
+    Serial.print(tempAndHumdiSensor.getTempOutside());
+    Serial.print(",");
+    Serial.print(tempAndHumdiSensor.getHumidity());
+    Serial.print(",");
+  }
+  else
+  {
+    Serial.println(NAN);
+  }
+  // ######### read siol sensors ########
 
-    // ########## check pump status #######
-    if (myPump.getIsOn())
-    {
-      Serial.println(myPump.getIsOn());
-    }
-    else if (myPump.getIsOff())
-    {
-      Serial.println(myPump.getIsOff());
-    }
-    else
-    {
-      Serial.println(NAN);
-    }
+  if (soilPinSensor.getSoilHumidPin1() || soilPinSensor.getSoilHumidPin2() || soilPinSensor.getSoilHumidPin3())
+  {
+    Serial.print(soilPinSensor.getSoilHumidPin1());
+    Serial.print(",");
+    Serial.print(soilPinSensor.getSoilHumidPin2());
+    Serial.print(",");
+    Serial.print(soilPinSensor.getSoilHumidPin3());
+    Serial.print(",");
+  }
+  else
+  {
+    Serial.println(NAN);
+    delay(2000);
+  }
 
-    // ######### read siol sensors ########
-    if (soilPinSensor.getSoilHumidPin1() || soilPinSensor.getSoilHumidPin2() || soilPinSensor.getSoilHumidPin3())
-    {
-      Serial.println(soilPinSensor.getSoilHumidPin1());
-      Serial.println(soilPinSensor.getSoilHumidPin2());
-      Serial.println(soilPinSensor.getSoilHumidPin3());
-    }
-    else
-    {
-      Serial.println(NAN);
-    }
+  // ####### check fan status ########
+
+  if (actuatorFan.getIsOn())
+  {
+    Serial.print(actuatorFan.getIsOn());
+    Serial.print(",");
+  }
+  else if (actuatorFan.getIsOff())
+  {
+    Serial.print(actuatorFan.getIsOff());
+    Serial.print(",");
+  }
+  else
+  {
+    Serial.println(NAN);
+    Serial.print(",");
+  }
+
+  // ########## check pump status #######
+
+  if (actuatorPump.getIsOn())
+  {
+    Serial.println(actuatorPump.getIsOn());
+  }
+  else if (actuatorPump.getIsOff())
+  {
+    Serial.println(actuatorPump.getIsOff());
+  }
+  else
+  {
+    Serial.println(NAN);
   }
 
   // ######## Pump control by Humdimity sensors ##################
   if (soilPinSensor.read())
   {
-    if (soilPinSensor.getSoilHumidPin1() || soilPinSensor.getSoilHumidPin2() || soilPinSensor.getSoilHumidPin3() <= 60)
+    if ((soilPinSensor.getSoilHumidPin1() <= 60 || soilPinSensor.getSoilHumidPin2() <= 60 || soilPinSensor.getSoilHumidPin3()) <= 60)
     {
-      myPump.turnOn();
+      actuatorPump.turnOn();
       delay(5000);
     }
     else
     {
-      myPump.turnOff();
+      actuatorPump.turnOff();
     }
   }
+
   // ######## Fans control by temperture sensor in Greenhouse ##################
-  if (mySensors.read())
+  if (tempAndHumdiSensor.read())
   {
-    if (mySensors.getTempInside() > 27)
+    if (tempAndHumdiSensor.getTempInside() > 27)
     {
-      myFan.turnOn();
+      actuatorFan.turnOn();
     }
     else
     {
-      myFan.turnOff();
+      actuatorFan.turnOff();
     }
   }
+  delay(2000);
 }
